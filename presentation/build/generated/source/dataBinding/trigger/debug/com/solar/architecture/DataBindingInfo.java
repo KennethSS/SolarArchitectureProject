bd315ebed85640404f820d0fa450d0921d4bc9b8
kenneth <wlsrhkd4023@gmail.com>
2020-08-20 15:28:09 +0900

    remove: dagger build file

--- presentation/build/generated/source/dataBinding/trigger/debug/com/solar/architecture/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.solar.architecture;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- presentation/build/generated/source/kapt/debug/com/solar/architecture/BR.java
+++
@@ -1,7 +0,0 @@
-package com.solar.architecture;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int text = 1;
-}
