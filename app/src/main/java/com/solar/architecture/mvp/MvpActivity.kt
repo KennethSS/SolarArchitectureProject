c2d83310e6f68ebba8b22fc2e77a51e6d0e1346b
kenneth <wlsrhkd4023@gmail.com>
2020-08-20 11:10:09 +0900

    change module name

--- app/src/main/java/com/solar/architecture/mvp/MvpActivity.kt
+++ app/src/main/java/com/solar/architecture/mvp/MvpActivity.kt
@@ -6,5 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MvpActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mvp)
     }
 }
